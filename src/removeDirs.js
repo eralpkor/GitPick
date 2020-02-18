480fa0c734981913cb6d9b07be16c88efa429a83
eralpkor <eralp.ny@gmail.com>
2020-02-18 12:20:12 -0500

    cleaned code

--- src/removeDirs.js
+++ src/removeDirs.js
@@ -13,11 +13,11 @@ function getFilePath(dir) {
 (function deleteDirectories() {
     students.forEach(s => {
       let userDirectory = getFilePath(s.dir);
-      console.log(`Deleting ${s.dir}`)
+      console.log(`\x1b[31mEverything will be deleted in\x1b[31m \x1b[33m${s.dir} \x1b[31m directory.\x1b[31m`)
       rmdir(userDirectory, (e) => {
         let names = s.dir.charAt(0).toUpperCase() + s.dir.slice(1);
         console.log(e)
-        console.log(`\x1b[35m\From user name ${names} everything deleted...\x1b[0m`)
+        console.log(`\x1b[32m\From user name ${names} everything deleted...\x1b[0m`)
       });
     })
 })();
